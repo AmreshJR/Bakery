2b82b115bafaeacaf65fa81a60eebc63c4a63a64
AmreshJR <55473983+AmreshJR@users.noreply.github.com>
2021-11-02 15:10:17 +0530

    New Src files plus product page layout change

--- ComponentProduct/main.js
+++ ComponentProduct/main.js
@@ -1,107 +1,110 @@
 let storeData = [
     {
-        ProductID : "Pudding",
-        ProductValue: "Wipped Cream, Vanila",
+        ProductID: "Chocolate Muffin",
+        ProductValue: "Calling all chocoholics! These tender chocolate muffins with a double hit of chocolate make a delicious breakfast.",
         ProductSrc: "../src/Muffin.jpg"
     },
-   
+
     {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
-    },
-    {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
+        ProductID: "Chocolate Pudding Sundaes",
+        ProductValue: "This chocolate pudding is silky-smooth and intensely chocolate — a world apart from instant or store bought.",
+        ProductSrc: "../src/Pudding.png"
     },
     {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
+        ProductID: "Chocolate Crinkle Cookies",
+        ProductValue: "Crisp on the outside and fudgy on the inside, these chocolate crinkle cookies are like brownies in cookie form.",
+        ProductSrc: "../src/Chocolate-Crinkle-Cookies.jpg"
     },
     {
-        ProductID : "Pudding",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
-    },
-   
-    {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
+        ProductID: "Strawaberry Cake",
+        ProductValue: "Jam-packed with fresh strawberries, this strawberry cake is one of the simplest, most delicious cakes you’ll ever make.",
+        ProductSrc: "../src/Strawberry-Cake.jpg"
     },
     {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
+        ProductID: "Chocolate Chip Muffins",
+        ProductValue: "A kid favorite, these chocolate chip muffins taste like chocolate chip cookies in muffin form.",
+        ProductSrc: "../src/ChocoMuffins.jpg"
+    },
+
+    {
+        ProductID: "Sour Cream Cake",
+        ProductValue: "Rich and tender from the addition of sour cream, and jam-packed with chocolate chips, this is loved by all.",
+        ProductSrc: "../src/sourCake.jpg"
     },
     {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
+        ProductID: "Pecan Shortbread Cookies",
+        ProductValue: "Rolled in coarse sugar, these sophistic pecan shortbread cookies are long on buttery, nutty flavor.",
+        ProductSrc: "../src/pecan.jpg"
     },
     {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
-    },
-    {
-        ProductID : "Muffin",
-        ProductValue: "Rs. 100",
-        ProductSrc: "../src/Muffin.jpg"
-    },
+        ProductID: "Molten Chocolate Cakes",
+        ProductValue: "Molten chocolate cakes also known as lava cakes, are rich individual chocolate cakes with oozing molten centers.",
+        ProductSrc: "../src/Lava.jpg"
+    }
 ]
 
-showCart = (data)=>{
-data.forEach(e => {
-    let main = document.querySelector(".products"); //Access Product Div
-    let newCard= document.createElement("div"); // Create a new div under product
-    newCard.className = "product-card"; //Add class name
-    let newImg = document.createElement("div"); // create a new div under product-card
-    newImg.className = "product-image";
-    let srcImg = document.createElement("img");
-    srcImg.src=e.ProductSrc;
-    let productName = document.createElement("div");
-    productName.className = "product-info";
-    let productInfo_1= document.createElement("h5");
-    productInfo_1.className = "text-head";
-    productInfo_1.textContent = e.ProductID;
-    productName.appendChild(productInfo_1);
-    let productIndo_2=document.createElement("p");
-    productIndo_2.className = "text-content";
-    productIndo_2.textContent = e.ProductValue;
-    productName.appendChild(productIndo_2);
-    newImg.appendChild(srcImg);
-    newCard.appendChild(newImg);
-    newCard.appendChild(productName);
-    main.appendChild(newCard);
-});
-   
+showCart = (data) => {
+    data.forEach(e => {
+        let main = document.querySelector(".products"); //Access Product Div
+        let newCard = document.createElement("div"); // Create a new div under product
+        newCard.className = "product-card"; //Add class name
+        let newImg = document.createElement("div"); // create a new div under product-card
+        newImg.className = "product-image";
+        let srcImg = document.createElement("img");
+        srcImg.src = e.ProductSrc;
+        let productName = document.createElement("div");
+        productName.className = "product-info";
+        let productInfo_1 = document.createElement("h5");
+        productInfo_1.className = "text-head";
+        productInfo_1.textContent = e.ProductID;
+        
+        let productIndo_2 = document.createElement("p");
+        productIndo_2.className = "text-content";
+        productIndo_2.textContent = e.ProductValue;
+        productName.appendChild(productIndo_2);
+        newImg.appendChild(srcImg);
+        newCard.appendChild(newImg);
+        newCard.appendChild(productInfo_1);
+        newCard.appendChild(productName);
+        main.appendChild(newCard);
+    });
+
 }
 
 showCart(storeData);
 
-filter=()=>{
+filter = () => {
     let val = document.getElementById("filter-search").value;
     console.log(val);
     let main = document.querySelector(".products");
-    main.innerHTML="";
-  showCart(storeData.filter(e=>{
-      if(val == "")
-      {     document.getElementById("btn-search").style.display= "none";
-          return e}
-      else
-      {   document.getElementById("btn-search").style.display= "block";
-          return e.ProductID == val;}
-}))  
+    main.innerHTML = "";
+    showCart(storeData.filter(e => {
+        if (val == "") {
+            document.getElementById("btn-search").style.display = "none";
+            return e
+        }
+        else {
+            document.getElementById("btn-search").style.display = "block";
+            let result = e.ProductID.toLowerCase();
+            return result.includes(val.toLowerCase());;
+        }
+    }))
 }
 
-removeFilter=()=>{
+removeFilter = () => {
     let main = document.querySelector(".products");
-    main.innerHTML="";
-    document.getElementById("filter-search").value="";
+    main.innerHTML = "";
+    document.getElementById("filter-search").value = "";
     showCart(storeData);
-    document.getElementById("btn-search").style.display= "none";
+    document.getElementById("btn-search").style.display = "none";
 
 }
+for (var item of document.querySelectorAll(".text-head")) {
+    item.addEventListener("click", function () {
+        console.log("hello");
+        url = 'http:///?name=' + encodeURIComponent(b);
+        document.location.href = url;
+        window.location.href = "http://www.google.com";
+    });
+}
+
